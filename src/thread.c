3cdb8377650b68d7f53a732d52c6deb036b858d6
nicvidal <nicvidal@enseirb-matmeca.fr>
2017-04-04 17:27:55 +0200

    implementation of thread_yield : not tested

--- src/thread.c
+++ src/thread.c
@@ -1,14 +1,21 @@
+#include "queue.h"
+#include "o_queue.h"
+//push_back, pop, get
+
+
 thread_t thread_self(void)
 {
 
 }
 
- int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){
+int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){
 
- }
+}
 
 int thread_yield(void){
-
+	tthread_t actual = pop();
+	push_back(actual);
+	swap_context(second()._context, first()._context);		
 }
 
 
