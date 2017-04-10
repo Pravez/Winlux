c56b3c52e7e0788acdb17d107f32366c72b3766c
VIDAL Nicolas <nicvidal@mitraille.pedago.ipb.fr>
2017-04-10 11:29:38 +0200

    add comments and correct thread.c

--- src/thread.c
+++ src/thread.c
@@ -9,14 +9,19 @@
 
 #define TO_TTHREAD(void_ptr) ((struct tthread_t*)void_ptr)
 
-
+/*
+* Récupère l'identifiant du thread courant.
+*/
 thread_t thread_self(void)
 {
     struct tthread_t * current = queue__first();
     return (thread_t) current;
 }
 
-
+/*
+ * Créé un nouveau thread qui va exécuter la fonction func avec l'argument funcarg.
+ * Renvoie 0 en cas de succès, -1 en cas d'erreur.
+ */
 int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg) {
   struct watchdog_args args;
 
@@ -38,14 +43,20 @@ int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg) {
   // check if the main has been put in a thread
 }
 
-
+/*
+ * Passe la main à un autre thread.
+ */
 int thread_yield(void){
     struct tthread_t * actual = queue__pop();
 	queue__push_back(actual);
 	swapcontext(&TO_TTHREAD(queue__second())->_context, &TO_TTHREAD(queue__first())->_context);
 }
 
-
+/*
+ * Attend la fin d'exécution d'un thread.
+ * La valeur renvoyée par le thread est placée dans *retval.
+ * Si retval est NULL, la valeur de retour est ignorée.
+ */
 int thread_join(thread_t thread, void **retval){
 	if (thread == NULL) { //doesn't exist --> error, invalid
 		perror("Error : thread doesn't exist in thread_join");
@@ -57,6 +68,7 @@ int thread_join(thread_t thread, void **retval){
 	if (tthread->_state == ACTIVE) {
         tthread->_join_wait++; //increment the number of thread that wait the thread
 		thread_yield(); //give the hand
+  }
 
 
     int i =0;
@@ -73,7 +85,10 @@ int thread_join(thread_t thread, void **retval){
     }
 }
 
-
+/*
+ * Termine le thread courant en renvoyant la valeur de retour retval.
+ * Cette fonction ne retourne jamais.
+ */
 void thread_exit(void *retval) __attribute__ ((__noreturn__)) {
   struct tthread_t * current = TO_TTHREAD(queue__first());
   current->retval = &retval; //pass function's retval to calling thread
