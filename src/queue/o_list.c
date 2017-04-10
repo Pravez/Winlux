5a53181fb95effc3a19715121aee11e8009ce8af
pravez <paul.ravezb@gmail.com>
2017-04-10 12:02:57 +0200

    Continued to modify list, added some functions and modified thread_join

--- src/queue/o_list.c
+++ src/queue/o_list.c
@@ -1,39 +1,38 @@
 #include "o_list.h"
 
-struct node * createnode(void* data){
-    struct node * newNode = malloc(sizeof(struct node));
+struct node *createnode(void *data) {
+    struct node *newNode = malloc(sizeof(struct node));
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
 }
 
-struct list * emptylist(){
-    struct list * list = malloc(sizeof(struct list));
+struct list *emptylist() {
+    struct list *list = malloc(sizeof(struct list));
     list->head = NULL;
     return list;
 }
 
-void add(void* data, struct list* list){
-    struct node * current = NULL;
-    if(list->head == NULL){
+void add(void *data, struct list *list) {
+    struct node *current = NULL;
+    if (list->head == NULL) {
         list->head = createnode(data);
-    }
-    else {
+    } else {
         current = list->head;
-        while (current->next!=NULL){
+        while (current->next != NULL) {
             current = current->next;
         }
         current->next = createnode(data);
     }
 }
 
-void delete(void* data, struct list * list){
-    struct node * current = list->head;
-    struct node * previous = current;
-    while(current != NULL){
-        if(current->data == data){
+void delete(void *data, struct list *list) {
+    struct node *current = list->head;
+    struct node *previous = current;
+    while (current != NULL) {
+        if (current->data == data) {
             previous->next = current->next;
-            if(current == list->head)
+            if (current == list->head)
                 list->head = current->next;
             free(current);
             return;
@@ -43,14 +42,30 @@ void delete(void* data, struct list * list){
     }
 }
 
-int hasNext(struct node* node){
+void delete_node(struct node *node, struct list *list) {
+    struct node *current = list->head;
+    struct node *previous = current;
+    while (current != NULL) {
+        if (current == node) {
+            previous->next = current->next;
+            if (current == list->head)
+                list->head = current->next;
+            free(current);
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+}
+
+int hasNext(struct node *node) {
     return node->next == NULL;
 }
 
-void destroy(struct list * list){
-    struct node * current = list->head;
-    struct node * next = current;
-    while(current != NULL){
+void destroy(struct list *list) {
+    struct node *current = list->head;
+    struct node *next = current;
+    while (current != NULL) {
         next = current->next;
         free(current);
         current = next;
--- src/queue/o_list.h
+++ src/queue/o_list.h
@@ -18,5 +18,6 @@ void add(void* data, struct list * list);
 void delete(void* data, struct list * list);
 void destroy(struct list * list);
 int hasNext(struct node* node);
+void delete_node(struct node* node, struct list * list);
 
 #endif //FRED_CIE_O_LIST_H
